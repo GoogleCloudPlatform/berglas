41236f2e442293a3a5b759a587a9e77460ff0941
Seth Vargo <seth@sethvargo.com>
2019-04-19 14:40:19 -0400

    Document berglas package

--- berglas/berglas.go
+++ berglas/berglas.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package berglas is the Go API for calling berglas.
 package berglas
 
 import (
