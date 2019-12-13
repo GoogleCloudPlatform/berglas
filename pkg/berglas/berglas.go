30a64db608a1d61141f18fdb24b7fdf3cbc76f50
Seth Vargo <seth@sethvargo.com>
2019-12-13 17:40:37 -0500

    Bump for release

--- pkg/berglas/berglas.go
+++ pkg/berglas/berglas.go
@@ -41,7 +41,7 @@ const (
 	// Name, Version, ProjectURL, and UserAgent are used to uniquely identify this
 	// package in logs and other binaries.
 	Name       = "berglas"
-	Version    = "0.4.0"
+	Version    = "0.5.0"
 	ProjectURL = "https://github.com/GoogleCloudPlatform/berglas"
 	UserAgent  = Name + "/" + Version + " (+" + ProjectURL + ")"
 )
