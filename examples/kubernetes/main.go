de961f3b040b2740ba89d52cd516f3fa1ca65ad9
Nikola Milojević <9573356+nklmilojevic@users.noreply.github.com>
2020-07-13 14:12:39 +0200

    Restored back the correct repository

--- examples/kubernetes/main.go
+++ examples/kubernetes/main.go
@@ -17,7 +17,7 @@ import (
 const (
 	// berglasContainer is the default berglas container from which to pull the
 	// berglas binary.
-	berglasContainer = "gcr.io/berglas/berglas:latest"
+	berglasContainer = "us-docker.pkg.dev/berglas/berglas/berglas:latest"
 
 	// binVolumeName is the name of the volume where the berglas binary is stored.
 	binVolumeName = "berglas-bin"
